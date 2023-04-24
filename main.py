5adccf5e4a4cff060f6d1c2975089972018a445e
Wanderson Bueno <wanolibueno@gmail.com>
2023-04-24 14:16:24 -0300

    rearranging

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from dataclasses import dataclass
 
 class TokenClass(Enum):
     PALAVRA_RESERVADA = r"(struct\b|if\b|int\b|else\b|while\b|do\b|for\b|\bfloat\b|double\b|char\b|long\b|short\b|break\b|continue\b|case\b|switch\b|default\b|void\b|return)"
-    CONSTANTE_NUMERICA = r"(\d+(\.\d*)?|\.\d+)(?![a-zA-Z])"
-    IDENTIFICADOR = r"([a-zA-Z_][a-zA-Z0-9_]*|main|printf)"
-    CONSTANTE_TEXTO = r'\".*?\"'
     OPERADOR = r"(==|!=|<=|>=|\|\||&&|\+=|-=|\*=|\=|--|\+\+|\+|\/|->|\*|\-|\||!|&|%|<|>)"
     DELIMITADOR = r"\[|\]|\(|\)|\{|\}|\;|\,|\:"
+    CONSTANTE_NUMERICA = r"(\d+(\.\d*)?|\.\d+)(?![a-zA-Z])"
+    CONSTANTE_TEXTO = r'\".*?\"'
+    IDENTIFICADOR = r"([a-zA-Z_][a-zA-Z0-9_]*|main|printf)"
 
 
 @dataclass
@@ -21,7 +21,7 @@ class Token:
         return f'\n {self.token_class.name}: {self.token_value}'
 
 
-with open('testes/teste-erro1.c', 'r', encoding='utf-8') as f:
+with open('testes/teste5.c', 'r', encoding='utf-8') as f:
     code = f.read()
 
 code = re.sub(r'/\*[\s\S]*?\*/', '', code)
