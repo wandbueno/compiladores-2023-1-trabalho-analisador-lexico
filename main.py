8dd822c1641e18d6209d7f955b8cb20d165fe6c8
Wanderson Bueno <wanolibueno@gmail.com>
2023-04-25 16:20:30 -0300

    correção ponto_flutuante

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ class TokenClass(Enum):
     PALAVRA_RESERVADA = r"(struct\b|if\b|int\b|else\b|while\b|do\b|for\b|\bfloat\b|double\b|char\b|long\b|short\b|break\b|continue\b|case\b|switch\b|default\b|void\b|return)"
     OPERADOR = r"(==|!=|<=|>=|\|\||&&|\+=|-=|\*=|\=|--|\+\+|\+|\/|->|\*|\-|\||!|&|%|<|>)"
     DELIMITADOR = r"\[|\]|\(|\)|\{|\}|\;|\,|\:"
-    CONSTANTE_NUMERICA = r"(\d+(\.\d*)?|\.\d+)(?![a-zA-Z])"
     CONSTANTE_TEXTO = r'\".*?\"'
+    PONTO_FLUTUANTE = r'(\d+\.\d+)'
+    CONSTANTE_INTEIRA = r"(\d+)(?![a-zA-Z])"
     IDENTIFICADOR = r"([a-zA-Z_][a-zA-Z0-9_]*|main|printf)"
 
 
